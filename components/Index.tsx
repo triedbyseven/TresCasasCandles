1195a57f3d0038512de71512ed5148f439c2bc4b
triedbyseven <mikedev0431@gmail.com>
2020-09-11 11:10:48 -0700

    Components folder with Index component created

---
+++ components/Index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View } from "react-native";
+
+export interface IndexProps {
+    
+}
+ 
+const Index: React.SFC<IndexProps> = () => {
+    return ( 
+        <View />
+     );
+}
+ 
+export default Index;
