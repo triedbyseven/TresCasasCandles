2b44a8e708dc989ffd68af282322728a23c1066f
triedbyseven <mikedev0431@gmail.com>
2020-09-11 12:23:43 -0700

    Added stack navigator and import of Home for component to navigate too for the first home screen.

--- components/Index.tsx
+++ components/Index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View } from "react-native";
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
 
-export interface IndexProps {
-    
-}
+export interface IndexProps {}
+
+const Stack = createStackNavigator();
  
 const Index: React.SFC<IndexProps> = () => {
     return ( 
-        <View />
+        <Stack.Navigator>
+            <Stack.Screen name="Home" component={Home} />
+        </Stack.Navigator>
      );
 }
  
