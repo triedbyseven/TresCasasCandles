8181a58765ccb8c3a5d95599cac0340047d68d0e
triedbyseven <mikedev0431@gmail.com>
2020-09-11 10:53:24 -0700

    App now passes withtout crashing, no http link was causing this to crash.

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -5,10 +5,16 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import { MockedProvider } from '@apollo/client/testing';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
 
 it('renders correctly', () => {
-  renderer.create(<App />);
+  renderer.create(
+    <MockedProvider>
+      <App />
+    </MockedProvider>
+    
+  );
 });
