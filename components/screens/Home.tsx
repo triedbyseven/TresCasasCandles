2061826969290677c43112f6177b0b6a5bd29caa
triedbyseven <mikedev0431@gmail.com>
2020-09-11 12:26:01 -0700

    New home component for the home screen

---
+++ components/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from "react-native";
+
+export interface HomeProps {
+    
+}
+ 
+const Home: React.SFC<HomeProps> = () => {
+    return ( <View /> );
+}
+ 
+export default Home;
