43aa2feae513303e338c63adbd5fae821731a7ac
triedbyseven <mikedev0431@gmail.com>
2020-09-11 13:34:10 -0700

    Added initialazation for StyleSheet with one container object. This conatains a soft pink for background color and flex. The main view has these style applied now.

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export interface HomeProps {
     
 }
  
 const Home: React.SFC<HomeProps> = () => {
-    return ( <View /> );
+    return ( 
+    <View style={styles.container} />
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#EBC3CB'
+    }
+});
  
 export default Home;
