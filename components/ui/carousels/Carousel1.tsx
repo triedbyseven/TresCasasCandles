d29e661cdd66ed1979308ca77e5c37f448dc3095
triedbyseven <mikedev0431@gmail.com>
2020-09-11 14:35:17 -0700

    Added new carousels folder to contain all the new ui carousels we create

---
+++ components/ui/carousels/Carousel1.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View } from "react-native";
+
+export interface Carousel1Props {}
+ 
+const Carousel1: React.SFC<Carousel1Props> = () => {
+    return ( 
+        <View />
+     );
+}
+ 
+export default Carousel1;
