bfe3d30de1866073776379c76f4dec171b7c9145
triedbyseven <mikedev0431@gmail.com>
2020-09-10 22:55:33 -0700

    Added entry point for react navigation with NavigationContainer

--- App.tsx
+++ App.tsx
@@ -8,19 +8,19 @@
  * @format
  */
 
+import 'react-native-gesture-handler';
 import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-} from 'react-native';
+import { SafeAreaView, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = () => {
   return (
     <>
+    <NavigationContainer>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView>
-        
       </SafeAreaView>
+    </NavigationContainer>
     </>
   );
 };
