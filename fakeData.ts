bbb12680a92512baa256c22925e855def2f548d6
triedbyseven <mikedev0431@gmail.com>
2020-09-12 13:45:13 -0700

    Added array of fade data to this module to mimic database values

---
+++ fakeData.ts
@@ -0,0 +1,11 @@
+interface FakeData {
+    id: number;
+    name: string;
+}
+
+export const fakeData: FakeData[] = [
+    { id: 1, name: 'michael' }, 
+    { id: 2, name: 'adrie' }, 
+    { id: 3, name: 'dude' }, 
+    { id: 4, name: 'awesome' }
+];
