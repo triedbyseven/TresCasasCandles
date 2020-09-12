8866d290d4a8475799ca8ff9a85f46ffe1b12b91
triedbyseven <mikedev0431@gmail.com>
2020-09-12 15:17:30 -0700

    Added UI components to display the item image, title and and price

--- components/ui/carousels/slides/Slide1.tsx
+++ components/ui/carousels/slides/Slide1.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View, Text, Image } from 'react-native';
 
 export interface Slide1Props {
     id: number;
@@ -8,8 +8,43 @@ export interface Slide1Props {
  
 const Slide1: React.SFC<Slide1Props> = ({ id, name }) => {
     return ( 
-        <View />
+        <View> 
+            <View style={{ width: '100%', height: '100%'}}>
+                <Image
+                    style={{ width: '100%', height: '100%' }}
+                    source={require('../../../../assets/Candle.G03.2k.png')}
+                />
+            </View>
+            <View style={{
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                width: '100%',
+                paddingHorizontal: 25,
+                marginBottom: 30,
+            }}>
+                <Text style={styles.title}>Gardenia scented candle</Text>
+                <Text style={styles.subTitle}>US $24.99</Text>
+            </View>
+        </View>
+
      );
-}
+};
+
+const styles = StyleSheet.create({
+    contentContainer: {
+        paddingHorizontal: 30,
+        marginBottom: 30
+    },
+    title: {
+        fontSize: 30,
+        color: '#fff',
+        marginBottom: 10
+    },
+    subTitle: {
+        fontSize: 18,
+        color: '#000'
+    }
+});
  
 export default Slide1;
