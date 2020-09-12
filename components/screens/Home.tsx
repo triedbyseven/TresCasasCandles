652f6f89d2e50f2af8ad021987ec777afa75635e
triedbyseven <mikedev0431@gmail.com>
2020-09-12 13:52:51 -0700

    Added imports for Heading1, Carousel & fakeData to render our sub components

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { StyleSheet, View } from "react-native";
+import Heading1 from '../ui/headings/Heading1';
+import Carousel1 from '../ui/carousels/Carousel1';
+import { fakeData } from '../../fakeData';
 
 export interface HomeProps {
     
@@ -7,13 +10,17 @@ export interface HomeProps {
  
 const Home: React.SFC<HomeProps> = () => {
     return ( 
-    <View style={styles.container} />
+    <View style={styles.container}> 
+        <Heading1 text="Featured"/>
+        <Carousel1 items={fakeData} />
+    </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        justifyContent: 'center',
         backgroundColor: '#EBC3CB'
     }
 });
