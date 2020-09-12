b7c330ee410e9fd3337e61d2a449a191529f2b51
triedbyseven <mikedev0431@gmail.com>
2020-09-12 14:03:57 -0700

    Test modified to pass with new props

--- __tests__/components/ui/carousels/Carosuel1-test.tsx
+++
@@ -1,9 +0,0 @@
-import 'react-native';
-import React from 'react';
-import Carousel1 from '../../../../components/ui/carousels/Carousel1';
-
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-    renderer.create(<Carousel1 />);
-});
