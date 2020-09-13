5f3173d443263299a7b827227bb62ac1645238c9
triedbyseven <mikedev0431@gmail.com>
2020-09-12 19:33:20 -0700

    Added UI components , containers and a platform specific button using touchableOpacity and TouchableNativeFeedBack

--- components/ui/navigation/NavigationBar1.tsx
+++ components/ui/navigation/NavigationBar1.tsx
@@ -1,12 +1,51 @@
 import React from 'react';
-import { View } from "react-native";
+import { Platform, StyleSheet, View, TouchableOpacity, TouchableNativeFeedback, Text } from "react-native";
 
 export interface NavigationBar1Props {
     
 }
  
 const NavigationBar1: React.SFC<NavigationBar1Props> = () => {
-    return ( <View /> );
+    return ( 
+        <View style={styles.container}>
+            <View style={styles.innerContainer}>
+                {Platform.OS === 'ios' ?
+                    <TouchableOpacity style={styles.button}>
+                        <Text>Home</Text>
+                    </TouchableOpacity> :
+                    <TouchableNativeFeedback
+                        onPress={() => console.log('Pressed')}
+                        useForeground={true}
+                        delayPressIn={0}
+                        background={TouchableNativeFeedback.Ripple('rgba(0,0,0.45)', false)}
+                    >
+                        <View style={styles.button}>
+                            <Text>Home</Text>
+                        </View>
+                    </TouchableNativeFeedback>}
+            </View>
+            
+        </View>
+    );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#fff',
+        paddingVertical: 30,
+        paddingHorizontal: 40,
+        borderTopLeftRadius: 40,
+        borderTopRightRadius: 40,
+    },
+    innerContainer: {
+        flexDirection: 'row'
+    },
+    button: {
+        overflow: 'hidden',
+        paddingVertical: 10,
+        paddingHorizontal: 10,
+        borderRadius: 30,
+    }
+});
  
 export default NavigationBar1;
