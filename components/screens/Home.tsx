fa511e7ea6db85abe9685f71f8dc4b046d4243c8
triedbyseven <mikedev0431@gmail.com>
2020-09-12 19:35:48 -0700

    Adde Navigation1 component. Cleaned up styling and added a wrapper around heading and carousel with a flex of 1. This container will be used to view all of the main content.

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { StyleSheet, View } from "react-native";
 import Heading1 from '../ui/headings/Heading1';
 import Carousel1 from '../ui/carousels/Carousel1';
+import NavigationBar1 from '../ui/navigation/NavigationBar1';
 import { fakeData } from '../../fakeData';
 
 export interface HomeProps {
@@ -10,17 +11,19 @@ export interface HomeProps {
  
 const Home: React.SFC<HomeProps> = () => {
     return ( 
-    <View style={styles.container}> 
-        <Heading1 text="Featured"/>
-        <Carousel1 items={fakeData} />
-    </View>
+        <View style={styles.container}> 
+            <View style= {{ flex: 1 }}>
+                <Heading1 text="Featured"/>
+                <Carousel1 items={fakeData} />
+            </View>
+            <NavigationBar1 />
+        </View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
+        height: '100%',
         backgroundColor: '#EBC3CB'
     }
 });
