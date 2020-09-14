b21233d87fd2ec5aeb150e8b4b0add12bb484c34
triedbyseven <mikedev0431@gmail.com>
2020-09-14 13:57:51 -0700

    Added new menu item data for our NavigationBar1 component.

---
+++ data/components/ui/navigation/navigationbar1-data.ts
@@ -0,0 +1,33 @@
+interface MenuItems {
+    index: number;
+    id: number;
+    title: string;
+};
+
+export const menuItems: MenuItems[] = [
+    {
+        index: 0,
+        id: 1,
+        title: 'Home'
+    },
+    {
+        index: 1,
+        id: 2,
+        title: 'Search'
+    },
+    {
+        index: 2,
+        id: 3,
+        title: 'Favorites'
+    },
+    {
+        index: 3,
+        id: 4,
+        title: 'Alerts'
+    },
+    {
+        index: 4,
+        id: 5,
+        title: 'Profile'
+    }
+];
