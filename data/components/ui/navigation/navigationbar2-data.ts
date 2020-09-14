6e263694c51fc27d27ca769f210b34584f7efd27
triedbyseven <mikedev0431@gmail.com>
2020-09-14 09:50:22 -0700

    Added new data folder to hold specific module data.

---
+++ data/components/ui/navigation/navigationbar2-data.ts
@@ -0,0 +1,39 @@
+interface MenuItems {
+    index: number;
+    id: number;
+    title: string;
+};
+
+export const menuItems: MenuItems[] = [
+    { 
+        index: 0,
+        id: 1,
+        title: 'Coat'
+     },
+    {
+        index: 1,
+        id: 2,
+        title: 'T-Shirt'
+    },
+    {
+        index: 2,
+        id: 3,
+        title: 'Jacket'
+    },
+    {
+        index: 3,
+        id: 4,
+        title: 'Suit'
+    },
+    {
+        index: 4,
+        id: 5,
+        title: 'Watch'
+    },
+    {
+        index: 6,
+        id: 7,
+        title: 'Sweater'
+    }
+
+];
