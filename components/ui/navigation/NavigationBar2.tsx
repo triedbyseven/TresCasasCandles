7eed15964ccfefedbe91308701e9c9ea7e5a15b5
triedbyseven <mikedev0431@gmail.com>
2020-09-14 13:31:04 -0700

    Added new UI, styles, react hooks & animated values to create a fluid marker shared market component that will help users to know what featured category they are currently viewing.

--- components/ui/navigation/NavigationBar2.tsx
+++ components/ui/navigation/NavigationBar2.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { StyleSheet, View, Animated } from 'react-native';
+import React, { useRef, useEffect, useState } from 'react';
+import { Animated, StyleSheet, View } from 'react-native';
 import NavButton2 from '../buttons/NavButton2';
 import { menuItems } from '../../../data/components/ui/navigation/navigationbar2-data';
 
-export interface NavigationBar2Props {
-    
-};
+export interface NavigationBar2Props {};
  
 const NavigationBar2: React.SFC<NavigationBar2Props> = () => {
+    const [state, updateState] = useState({ xCoord: 0, width: 0 });
+    const widthAnim = useRef(new Animated.Value(0)).current;
+    const barAnim = useRef(new Animated.ValueXY()).current;
+
+    const animate = (layout: any): void => {
+        const { xCoord, width } = layout;
+
+        Animated.parallel([
+            Animated.spring(barAnim, {
+                useNativeDriver: false,
+                toValue: {
+                    x: xCoord,
+                    y: 0
+                },
+                speed: 20,
+            }),
+            Animated.timing(widthAnim, {
+                useNativeDriver: false,
+                toValue: width - 44,
+                duration: 250
+            })
+        ]).start()
+    };
+
+    useEffect(() => {
+        animate(state)
+    },[state])
+
     return ( 
         <View style={styles.container}>
             <View style={styles.innerContainer}>
@@ -15,9 +41,14 @@ const NavigationBar2: React.SFC<NavigationBar2Props> = () => {
                     horizontal={true}
                     showsHorizontalScrollIndicator={false}
                     style={styles.scrollView}
-                    contentContainerStyle={{ paddingLeft: 20 }}
+                    contentContainerStyle={styles.contentContainerStyle}
                 >
-                    {menuItems.map(({index, title}) => <NavButton2 key={index} text={title} />)}
+                    <View>
+                        <View style={styles.scrollView}>
+                             {menuItems.map(({index, title}) => <NavButton2 key={index} index={index} text={title} updater={updateState} />)}
+                        </View>
+                        <Animated.View style={[styles.marker, barAnim.getLayout(), {width: widthAnim}]} />
+                    </View>
                 </Animated.ScrollView>
             </View>
         </View> 
@@ -32,8 +63,16 @@ const styles = StyleSheet.create({
     innerContainer: {
         paddingVertical: 5,
     },
+    contentContainerStyle: {
+        paddingLeft: 20 
+    },
     scrollView: {
         flexDirection: 'row'
+    },
+    marker: {
+        height: 2,
+        backgroundColor: 'black',
+        marginHorizontal: 22,
     }
 });
  
