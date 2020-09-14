d8098ce42d977b2f085c5c4d5be99f7258d0396d
triedbyseven <mikedev0431@gmail.com>
2020-09-14 09:24:25 -0700

    Added SearchBar1 to UI view and refactored inline styles into their own style objeccts

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -5,6 +5,7 @@ import Heading1 from '../ui/headings/Heading1';
 import Carousel1 from '../ui/carousels/Carousel1';
 import NavigationBar1 from '../ui/navigation/NavigationBar1';
 import NavigationBar2 from '../ui/navigation/NavigationBar2';
+import SearchBar1 from '../ui/search/SearchBar1';
 
 export interface HomeProps {
     
@@ -13,10 +14,13 @@ export interface HomeProps {
 const Home: React.SFC<HomeProps> = () => {
     return ( 
         <View style={styles.container}> 
-            <View style= {{ flex: 1 }}>
+            <View style= {styles.innerContainer}>
+                <SearchBar1 />
                 <NavigationBar2 />
-                <Heading1 text="Featured"/>
-                <Carousel1 items={fakeData} />
+                <View style={styles.featuredContainer}>
+                    <Heading1 text="Featured"/>
+                    <Carousel1 items={fakeData} />
+                </View>
             </View>
             <NavigationBar1 />
         </View>
@@ -27,6 +31,12 @@ const styles = StyleSheet.create({
     container: {
         height: '100%',
         backgroundColor: '#EBC3CB'
+    },
+    innerContainer: {
+        flex: 1
+    },
+    featuredContainer: {
+        flex: 0.8
     }
 });
  
