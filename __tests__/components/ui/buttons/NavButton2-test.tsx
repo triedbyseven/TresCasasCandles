ecc011edfbf5ea40304c36bc698f100ccde7f2a8
triedbyseven <mikedev0431@gmail.com>
2020-09-14 13:16:36 -0700

    Added new UI to handle passing layout props for new TouchableOpacity component into local state. UseEffect was also added to handle updating the initial xCoords and width when the component loads. New index and updater props have been added to work with keeping track of whcih button to send layout props into parent component

--- __tests__/components/ui/buttons/NavButton2-test.tsx
+++ __tests__/components/ui/buttons/NavButton2-test.tsx
@@ -5,11 +5,15 @@ import NavButton2 from '../../../../components/ui/buttons/NavButton2';
 import renderer from 'react-test-renderer';
 
 it('renders without crashing', () => {
-    renderer.create(<NavButton2 text="Label" />);
+    const updateFunction = () => console.log('Mock Function');
+
+    renderer.create(<NavButton2 text="Label" index={0} updater={updateFunction} />);
 });
 
-it('takes a text prop', () => {
-    const testRenderer = renderer.create(<NavButton2 text='Label' />);
+it('takes a text, index, updater as props', () => {
+    const updateFunction = () => console.log('Mock Function');
+
+    const testRenderer = renderer.create(<NavButton2 text='Label' index={0} updater={updateFunction} />);
     const testInstance =  testRenderer.root;
-    expect(testInstance.props).toEqual({ text: 'Label' })
+    expect(testInstance.props).toEqual({ text: 'Label', index: 0, updater: updateFunction })
 });
--- components/ui/buttons/NavButton2.tsx
+++ components/ui/buttons/NavButton2.tsx
@@ -1,15 +1,30 @@
-import React from 'react';
-import { StyleSheet, View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, TouchableOpacity, View, Text } from 'react-native';
 
 export interface NavButton2Props {
     text: string;
+    index: number;
+    updater: any;
 }
  
-const NavButton2: React.SFC<NavButton2Props> = ({ text }) => {
+const NavButton2: React.SFC<NavButton2Props> = ({ text, index, updater }) => {
+    const [state, updateState] = useState({ xCoord: 0, width: 0 });
+
+    useEffect(() => {
+       if(index === 0) updater(state);
+    },[state]);
+
     return ( 
-        <View style={styles.menuItem}>
+        <TouchableOpacity 
+            style={styles.menuItem} 
+            onPress={() => updater(state)} 
+            onLayout={(event) => {
+                var { x, width } = event.nativeEvent.layout;
+                updateState({ xCoord: x, width: width });
+            }}
+        >
             <Text style={styles.text}>{text}</Text>
-        </View>
+        </TouchableOpacity>
     );
 }
 
