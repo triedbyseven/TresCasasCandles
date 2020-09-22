3cd0b06c27a381f9eeb2201d8118bc45851e48ba
triedbyseven <mikedev0431@gmail.com>
2020-09-21 23:03:20 -0700

    Added new Icons and text to for both the search box and the scan box

--- components/ui/search/SearchBar1.tsx
+++ components/ui/search/SearchBar1.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
+import Icon from 'react-native-vector-icons/Feather';
+import Icon2 from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export interface SearchBar1Props {
     
@@ -10,10 +12,26 @@ const SearchBar1: React.SFC<SearchBar1Props> = () => {
         <View style={styles.container}>
             <View style={styles.innerContainer}>
                 <View style={styles.searchContainer}>
-
+                    <View style={{ position: 'absolute', left: 14 }}>
+                        <Icon
+                            name='search'
+                            size={20}
+                            color='#C5CEE0'
+                        />
+                    </View>
+                    <Text style={{
+                        fontSize: 15, 
+                        color: '#8F9BB3', 
+                        paddingVertical: 16.75,
+                        paddingHorizontal: 16.75
+                    }}>Search</Text>
                 </View>
                 <View style={styles.activateBox}>
-
+                    <Icon2
+                        name='scan-helper'
+                        size={20}
+                        color='#000'
+                    />
                 </View>
             </View>
         </View> 
@@ -32,11 +50,15 @@ const styles = StyleSheet.create({
     },
     searchContainer: {
         flex: 1,
+        justifyContent: 'center',
         height: 52,
         backgroundColor: '#fff',
-        borderRadius: 6
+        borderRadius: 6,
+        paddingHorizontal: 28
     },
     activateBox: {
+        justifyContent: 'center',
+        alignItems: 'center',
         width: 48,
         height: 52,
         backgroundColor: '#fff',
