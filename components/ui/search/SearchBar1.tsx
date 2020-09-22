c8f87e2a928a1836fe64671f5d00d7808db50745
triedbyseven <mikedev0431@gmail.com>
2020-09-22 10:41:42 -0700

    Refactored code from SearchBar1 into ScanButton. These components are seperate and handle their individual UI elements

--- components/ui/search/SearchBar1.tsx
+++ components/ui/search/SearchBar1.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { StyleSheet, View, Text } from "react-native";
 import Icon from 'react-native-vector-icons/Feather';
-import Svg, { Path } from 'react-native-svg';
 import { BoxShadow } from 'react-native-shadow';
 
 export interface SearchBar1Props {
@@ -10,7 +9,6 @@ export interface SearchBar1Props {
  
 const SearchBar1: React.SFC<SearchBar1Props> = () => {
     const [searchWidth, updateSearchWidth] = useState(0);
-    const [scanWidth, updateScanWidth] = useState(0);
 
     const shadowOpt = {
         width: searchWidth === 0 ? 100 : searchWidth,
@@ -22,18 +20,6 @@ const SearchBar1: React.SFC<SearchBar1Props> = () => {
         x: 0,
         y: 5,
         style: { flex: 1, marginVertical: 0 }
-    };
-
-    const shadowOptScan = {
-        width: scanWidth === 0 ? 20 : scanWidth,
-        height: 48,
-        color: "#000",
-        border: 5,
-        radius: 3,
-        opacity: 0.03,
-        x: 0,
-        y: 5,
-        style: { marginVertical: 0, justifyContent: 'center', alignItems: 'center', }
     };
 
     return ( 
@@ -54,21 +40,6 @@ const SearchBar1: React.SFC<SearchBar1Props> = () => {
                     <Text style={styles.searchText}>Search</Text>
                 </View>
             </BoxShadow>
-            // <View style={styles.activateBox}
-            //     onLayout={event => {
-            //         var { width } = event.nativeEvent.layout;
-            //         updateScanWidth(width);
-            //     }}
-            // >
-            //     <BoxShadow setting={shadowOptScan}>
-            //             <Svg width={16} height={16} viewBox="0 0 16 16">
-            //                 <Path
-            //                     d="M10.667 0H16v4.444h-1.778V1.778h-3.555V0zM5.333 0v1.778H1.778v2.666H0V0h5.333zm5.334 16v-1.778h3.555v-2.666H16V16h-5.333zm-5.334 0H0v-4.444h1.778v2.666h3.555V16zM0 7.111h16V8.89H0V7.11z"
-            //                     fill="#000"
-            //                 />
-            //             </Svg>
-            //     </BoxShadow>
-            // </View>
 
     );
 };
@@ -85,15 +56,6 @@ const styles = StyleSheet.create({
     activateBoxIcon: {
         position: 'absolute',
         left: 14
-    },
-    activateBox: {
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: 48,
-        height: 48,
-        backgroundColor: '#fff',
-        marginLeft: 16,
-        borderRadius: 6
     },
     searchText: {
         fontSize: 15,
