9c8bb9dd570948cc01b1a76b959d25b303bacecf
triedbyseven <mikedev0431@gmail.com>
2020-09-21 21:05:01 -0700

    Added styles and component Ui along with Icon import. These were brought over from our Slide1.tsx file to help clean up code and modulearize the code base further.

--- components/ui/icons/IconHeart.tsx
+++ components/ui/icons/IconHeart.tsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import { View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export interface IconHeartProps {
     
 }
  
 const IconHeart: React.SFC<IconHeartProps> = () => {
-    return ( <View /> );
-}
+    return (
+        <View style={styles.container}>
+            <Icon
+                name="heart-outline"
+                size={20}
+                color="black"
+            />
+        </View>
+     );
+};
  
 export default IconHeart;
+
+const styles = StyleSheet.create({
+    container: {
+        position: 'absolute',
+        justifyContent: 'center',
+        alignItems: 'center',
+        top: 10,
+        right: 10,
+        width: 34,
+        height: 34,
+        backgroundColor: '#fff',
+        borderRadius: 30
+    }
+});
