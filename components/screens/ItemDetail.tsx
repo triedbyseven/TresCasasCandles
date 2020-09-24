7a89299d2dcec9f929af8a7b7f2d7ce998e4a034
triedbyseven <mikedev0431@gmail.com>
2020-09-24 09:00:37 -0700

    Added header UI and back icon with goBack functionality from react navigation 5. We have access to goBack via navigation props.

--- components/screens/ItemDetail.tsx
+++ components/screens/ItemDetail.tsx
@@ -1,12 +1,37 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, TouchableHighlight, View } from 'react-native';
+import Svg, { Path, Use, Defs} from 'react-native-svg';
 
 export interface ItemDetailProps {
-    
+    navigation: any;
 }
  
-const ItemDetail: React.SFC<ItemDetailProps> = () => {
-    return ( <View style={styles.container} /> );
+const ItemDetail: React.SFC<ItemDetailProps> = ({ navigation }) => {
+    return ( 
+        <View style={styles.container}> 
+            <View style={styles.headerContainer}>
+                <TouchableHighlight 
+                    activeOpacity={0.65}
+                    onPress={() => navigation.goBack()}
+                >
+                    <Svg width={20} height={16} viewBox="0 0 20 16">
+                        <Defs>
+                            <Path
+                                d="M14.582 7.866c.557.488.557 1.28 0 1.768l-4.704 4.115 13.693.001c.79 0 1.429.56 1.429 1.25s-.64 1.25-1.429 1.25l-13.695-.001 4.706 4.117c.557.488.557 1.28 0 1.768-.558.488-1.463.488-2.02 0l-7.144-6.25-.012-.011a1.335 1.335 0 01-.073-.071l.085.082a1.338 1.338 0 01-.26-.312 1.175 1.175 0 01-.113-.259 1.1 1.1 0 01.113-.886 1.05 1.05 0 01.096-.14 1.362 1.362 0 01.164-.17l7.143-6.25c.558-.489 1.463-.489 2.02 0z"
+                                id="prefix__a"
+                            />
+                        </Defs>
+                        <Use
+                            fill="#192038"
+                            xlinkHref="#prefix__a"
+                            transform="translate(-5 -7)"
+                            fillRule="evenodd"
+                        />
+                    </Svg>
+                </TouchableHighlight>
+            </View>
+        </View>
+    );
 }
  
 export default ItemDetail;
@@ -16,5 +41,9 @@ const styles = StyleSheet.create({
         flex: 1,
         height: '100%',
         backgroundColor: '#EDDEE1'
+    },
+    headerContainer: {
+        paddingHorizontal: 30,
+        paddingVertical: 7
     },
 });
