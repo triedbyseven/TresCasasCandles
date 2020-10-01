6ed413f2b83a2a85bcc6a4e20fc02c55960a14e4
triedbyseven <mikedev0431@gmail.com>
2020-10-01 11:47:00 -0700

    Adddd support for new interpolation of opacity. This prompted for a serperate animated value and custom parallelize method to run both interpolations in parrallel.

--- animations/colorAnimations.ts
+++ animations/colorAnimations.ts
@@ -1,28 +1,61 @@
-import { Animated } from 'react-native';
+import Animated, { Easing } from 'react-native-reanimated';
 
-const color = new Animated.Value(0);
+const { timing, interpolateColors, interpolate, Value } = Animated;
 let animateVar = false;
+const _color = new Value(0);
+const _color1 = new Value(0);
+
+const parallelize = (
+    animations: Animated.BackwardCompatibleWrapper[],
+    cb?: { onStart?: () => void; onDone?: () => void },
+) => {
+    cb?.onStart && cb.onStart();
+    const promises = animations.map(a => {
+        return new Promise(resolve => {
+            a.start(() => {
+                resolve();
+            });
+        });
+    });
+    return Promise.all(promises).then(() => {
+        cb?.onDone && cb.onDone();
+    });
+};
 
 const useColorAnimations: any = () => {
-
-    const colorInterpolation = color.interpolate({
+    const colorInterpolation = interpolateColors(_color, {
         inputRange: [0, 1],
-        outputRange: ['rgb(235,195,203)', 'rgb(236,222,225)']
+        outputColorRange: ['#EBC3CB', '#EDDEE1'],
+    });
+
+    const opacityInterpolation = interpolate(_color1, {
+        inputRange: [0, 1],
+        outputRange: [1.0, 0.0]
     });
     
-    const animateColor = ( animateChoice: boolean) => {
+    const animateColor = (animateChoice: boolean) => {
         animateVar = animateChoice;
 
-        Animated.timing(color, {
-            useNativeDriver: false,
-            toValue: animateVar ? 1 : 0,
-            duration: 0,
-        }).start();
+        parallelize(
+            [
+                timing(_color, {
+                    toValue: animateVar ? 1 : 0,
+                    duration: 1000,
+                    easing: Easing.inOut(Easing.ease)
+                }),
+                timing(_color1, {
+                    toValue: !animateVar ? 0 : 1,
+                    duration: 10,
+                    easing: Easing.inOut(Easing.ease)
+                }),
+            ],
+        );
     };
 
     return {
         animateColor,
         colorInterpolation,
+        opacityInterpolation,
     }
 
 };
