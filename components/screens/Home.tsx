358f3b2f4389c2c836fe6d8435b955dc1bac561f
triedbyseven <mikedev0431@gmail.com>
2020-10-01 11:43:05 -0700

    Added interpolation to the opacity of the entire view render. The idea here is to keep it from showing when the next route is loaded in. Without adjusting opacity we would still have visibility to this component. Refactored and cleaned up styles from being inlined.

--- components/screens/Home.tsx
+++ components/screens/Home.tsx
@@ -5,6 +5,8 @@ import { SearchContainer } from '../ui/search';
 import NavigationBar2 from '../ui/navigation/NavigationBar2';
 import Carousel1 from '../ui/carousels/Carousel1';
 import { fakeData } from '../../fakeData';
+import useColorAnimations from '../../animations/colorAnimations';
+import Animated from 'react-native-reanimated';
 
 export interface HomeProps {
     
@@ -12,16 +14,23 @@ export interface HomeProps {
  
 const Home: React.SFC<HomeProps> = () => {
     const insets = useSafeAreaInsets();
+    const { opacityInterpolation } = useColorAnimations();
+    const insetStyles = {
+        paddingTop: insets.top
+    }
+    const opacityStyles = {
+        opacity: opacityInterpolation
+    }
 
     return ( 
         <>
-            <StatusBar barStyle="dark-content" backgroundColor='#EBC3CB' />
-            <View style={{ flex: 0, paddingTop: insets.top, backgroundColor: '#EBC3CB'}} />
-            <View style= {styles.container}>
+            <StatusBar barStyle="dark-content" backgroundColor='rgba(0,0,0,0)' translucent={true} />
+            <View style={[styles.insetContainer, insetStyles]} />
+            <Animated.View style={[styles.container, opacityStyles]}>
                 <SearchContainer />
                 <NavigationBar2 />
                 <Carousel1 items={fakeData} />
-            </View>
+            </Animated.View>
         </>
      );
 }
@@ -29,9 +38,14 @@ const Home: React.SFC<HomeProps> = () => {
 export default Home;
 
 const styles = StyleSheet.create({
+    insetContainer: { 
+        flex: 0, 
+        backgroundColor: 'rgba(0,0,0,0)'
+    },
     container: {
         flex: 1,
         height: '100%',
-        backgroundColor: '#EBC3CB',
+        paddingBottom: 35,
+        backgroundColor: 'rgba(000,000,000,0.0)'
     },
 });
