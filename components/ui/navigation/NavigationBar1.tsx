b3003d97a8c63db2acf2f441539dab62c818f827
triedbyseven <mikedev0431@gmail.com>
2020-10-01 14:37:17 -0700

    Huge clean up here as alot of useRefs for animated values were polluting this module and have been moved into its own useNavigationAnimations hook to initialize and process these animations.

--- components/ui/navigation/NavigationBar1.tsx
+++ components/ui/navigation/NavigationBar1.tsx
@@ -1,9 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useRef, useState } from 'react';
 import { StyleSheet, View, Text } from "react-native";
 import Animated, { Easing } from 'react-native-reanimated';
 import NavButton1 from '../buttons/NavButton1';
 import { menuItems } from '../../../data/components/ui/navigation/navigationbar1-data';
-import { useRoute } from '@react-navigation/native';
+import { useNavigationAnimations, updateValue }  from '../../../animations';
 
 export interface NavigationBar1Props {
     navigation: any;
@@ -12,6 +12,8 @@ export interface NavigationBar1Props {
 const { Value, timing } = Animated;
  
 const NavigationBar1: React.FC<NavigationBar1Props> = ({ navigation }) => {
+    const { interpolateAnimatedHeight, interpolateMenuOpacity, interpolateItemOpacity, interpolateTranslateY } = useNavigationAnimations();
+
     const [state, updateState] = useState({ xCoord: 0, width: 0, height: 0, index: 0 });
     const [isAnimating, updateIsAnimating] = useState(false);
 
@@ -21,12 +23,6 @@ const NavigationBar1: React.FC<NavigationBar1Props> = ({ navigation }) => {
 
     const _ballWidthAnim = useRef(new Value(10)).current;
     const _ballHeightAnim = useRef(new Animated.Value(10)).current;
-
-    const _menuHeight = useRef(new Value(73)).current;
-    const _menuFade = useRef(new Value(1)).current;
-    const _itemDetailOpacity = useRef(new Value(0)).current;
-    const _itemDetailMargin = useRef(new Value(0)).current;
-
 
     const onPressMenuItem = (state: any, route: string) => {
         if(isAnimating) return;
@@ -142,90 +138,45 @@ const NavigationBar1: React.FC<NavigationBar1Props> = ({ navigation }) => {
         );
     };
 
-    const toggleMenuAnimation = (isToggled?: boolean) => {
-        const toggle = isToggled;
-
-        const _config_MenuHeight = {
-            duration: 250,
-            toValue: !toggle ? 371 : 73,
-            easing: Easing.inOut(Easing.ease),
-        };
-        const _config_MenuFade = {
-            duration: 250,
-            toValue: toggle ? 1 : 0,
-            easing: Easing.inOut(Easing.ease),
-        };
-        const _config_ItemDetailOpacity = {
-            duration: 250,
-            toValue: toggle ? 0 : 1,
-            easing: Easing.inOut(Easing.ease),
-        };
-        const _config_ItemDetailMarginY = {
-            duration: 250,
-            toValue: toggle ? 0 : -73 ,
-            easing: Easing.inOut(Easing.ease),
-        };
-
-        parallelize(
-            [
-                timing(_menuHeight, _config_MenuHeight),
-                timing(_menuFade, _config_MenuFade),
-                timing(_itemDetailOpacity, _config_ItemDetailOpacity),
-                timing(_itemDetailMargin, _config_ItemDetailMarginY),
-            ],
-            {
-                onStart: () => {
-                    console.log('started');
-                },
-                onDone: () => {
-                    console.log('finished')
-                },
-            },
-        );
-    };
-
-    const route = useRoute();
-
-    useEffect(() => {
-        // console.log(route?.state?.routes);
-        if (route?.state) {
-            if(route?.state?.routes[1]?.name === 'ItemDetail') return toggleMenuAnimation(false);
-
-            if (route?.state?.routes[0].name === 'Home' && route?.state?.routes.length === 1) return toggleMenuAnimation(true);
-        }
-    },[route]);
-
     return ( 
-        <Animated.View style={[styles.container, { height: _menuHeight }]}>
-            <Animated.View style={[styles.contentJustifyContainer, { opacity: _menuFade }]}>
-                <View style={styles.innerContainer}>
-                    {menuItems.map(({index, title, icon, route}) => <NavButton1 
-                            key={index} 
-                            text={title} 
-                            icon={icon} 
-                            index={index}
-                            route={route}
-                            onPressMenuItem={onPressMenuItem}
-                            currentMenuIndex={state.index}
-                            updater={updateState}
-                            animate={animate}
-                        />
-                    )}
-                </View>
-                <Animated.View style={[styles.markerContainer, { width: _widthAnim }, {
-                    transform: [
-                        { translateX: _selectorAnimX },
-                    ]
-                }]}>
-                    <Animated.View style={[styles.marker, { width: _ballWidthAnim, height: _ballHeightAnim }]} />
-                </Animated.View>
-            </Animated.View>
-            <Animated.View style={[
-                { opacity: _itemDetailOpacity, padding: 28 }, {
+    <Animated.View style={[styles.container, { height: interpolateAnimatedHeight }]}>
+            <Animated.View style={[styles.contentJustifyContainer, { opacity: interpolateMenuOpacity }]}>
+            <View style={styles.innerContainer}>
+                {menuItems.map(({index, title, icon, route}) => <NavButton1 
+                        key={index} 
+                        text={title} 
+                        icon={icon} 
+                        index={index}
+                        route={route}
+                        onPressMenuItem={onPressMenuItem}
+                        currentMenuIndex={state.index}
+                        updater={updateState}
+                        animate={animate}
+                    />
+                )}
+            </View>
+            <Animated.View style={[styles.markerContainer, { width: _widthAnim }, {
                 transform: [
-                    { translateY: _itemDetailMargin }
+                    { translateX: _selectorAnimX },
                 ]
             }]}>
+                <Animated.View style={[styles.marker, { width: _ballWidthAnim, height: _ballHeightAnim }]} />
+            </Animated.View>
+        </Animated.View>
+        <Animated.View 
+            style={[
+                { opacity: interpolateItemOpacity, padding: 28 }, {
+                transform: [
+                    { translateY: interpolateTranslateY }
+                ]
+            }]}
+        >
+            <View
+                onLayout={event => {
+                    if(event.nativeEvent.layout.height === 0) return;
+                    updateValue(event.nativeEvent.layout.height)
+                }}
+            >
                 <Text style={{ fontSize: 40, paddingRight: 40, color: '#222B45' }}>Oversized cotton coat</Text>
                 <Text style={{ fontSize: 18, color: '#DB2C66', marginVertical: 10, }}>US $149.89</Text>
                 <Text style={{ fontSize: 15, color: '#222B45', marginTop: 2.5, lineHeight: 24 }}>Oversized coat with lapel collar and long sleeves. Front double breasted button closure.</Text>
@@ -236,14 +187,16 @@ const NavigationBar1: React.FC<NavigationBar1Props> = ({ navigation }) => {
                 <View style={{ alignItems: 'center', backgroundColor: '#DB2C66', paddingVertical: 12, borderRadius: 30 }}>
                     <Text style={{ fontSize: 14, color: '#fff' }}>ADD TO CART</Text>
                 </View>
-            </Animated.View>
+            </View>
         </Animated.View>
+    </Animated.View>
     );
 }
 
 const styles = StyleSheet.create({
     container: {
         backgroundColor: '#fff',
+        marginTop: -35,
         paddingTop: 22,
         paddingBottom: 6,
         paddingHorizontal: 20,
